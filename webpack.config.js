2177a1c888869a08d53c62acc2a4cfa55b470ba3
Guilherme Antunes <guilherme.silva.antunes1999@gmail.com>
2022-02-24 19:37:26 -0300

    container esquerdo css feito

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,40 @@ module.exports = {
     }, {
       test: /\.css$/,
       use: ['style-loader', 'css-loader']
-    }]
+    },
+    {
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            name:'[name].[ext]',
+            outputPath: './src/assets/img/'
+          }
+        },
+        {
+          loader: 'image-webpack-loader',
+          options: {
+            query: {
+              mozjpeg: {
+                progressive: true,
+              },
+              gifsicle: {
+                interlaced: true,
+              },
+              optipng: {
+                optimizationLevel: 7,
+              }
+            }
+          }
+        }]
+    }
+    ]
   },
-  devtool: 'source-map'
+  devtool: 'source-map',
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  }
 };
