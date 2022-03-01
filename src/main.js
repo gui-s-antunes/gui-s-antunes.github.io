ee98ee67ef4394b26f86a059d850f3b13dbd6739
Guilherme Antunes <guilherme.silva.antunes1999@gmail.com>
2022-03-01 18:05:02 -0300

    feito relacao imagem descricao da home

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
 import './assets/css/style.css';
+
+(function(){
+    const description_imgs = document.querySelectorAll('[data-tab-target]');
+    const description_texts = document.querySelectorAll('[data-tab-content]');
+
+    description_imgs.forEach(description_img => {
+        description_img.addEventListener('click', e => {
+            description_imgs.forEach(description_img => {
+                description_img.classList.remove('mastering_img-selected');
+            })
+            description_texts.forEach(description_text => {
+                description_text.classList.remove('description_show');
+            });
+            description_img.classList.add('mastering_img-selected');
+            const text = document.querySelector(description_img.dataset.tabTarget);
+            text.classList.add('description_show');
+        });
+    });
+})();
