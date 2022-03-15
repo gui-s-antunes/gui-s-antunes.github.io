3c5c0ebce10b1f3571946813f35433361cfbec08
Guilherme Antunes <guilherme.silva.antunes1999@gmail.com>
2022-03-15 16:55:05 -0300

    carousel para imagens feito

---
+++ src/modules/Carousel.js
@@ -0,0 +1,33 @@
+class Carousel {
+    constructor(doms){
+        this.doms = doms;
+        this.position = 0;
+        this.arrayPosition = 0;
+    }
+
+    previousDom(){
+        this.arrayPosition--;
+        this.calcDom();
+    }
+    
+    nextDom(){
+        this.arrayPosition++;
+        this.calcDom();
+    }
+
+    calcDom(){
+        this.position = Math.abs(this.arrayPosition % this.doms.length);
+        this.setActiveDom();
+    }
+
+    setActiveDom(){
+        this.removeActive();
+        this.doms[this.position].classList.add('active');
+    }
+
+    removeActive(){
+        this.doms.forEach(dom => {
+            dom.classList.remove('active');
+        });
+    }
+}
